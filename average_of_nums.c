8f714cd4b9d03929f972311171912d30e686207a
Karanveer Singh <karanveerks2020@gmail.com>
2025-12-28 15:44:58 +0530

    Modify average calculation to use float array
    Updated the program to use float for numbers and added comments.

--- average_of_nums.c
+++ average_of_nums.c
@@ -3,19 +3,21 @@
 int main() {
     int entered, i;
     float average, sum=0.000;
-    int numbers[100];
-    printf("How many Numbers ?: ");
+    float numbers[50];
+    printf("How many Numbers ?: ");        // Specifying how many numbers user want.
     scanf("%d", &entered);
 
     for (i = 0; i < entered; i++) {
-        printf("Enter Number %d: ", i+1);
-        scanf("%d", &numbers[i]);
+        printf("Enter Number %d: ", i+1);        // Taking input from user
+        scanf("%f", &numbers[i]);
     }
     
-    for(i=0; i<entered; i++){
+    for(i=0; i<entered; i++){                // Adding numbers
         sum += numbers[i];
     }
-    average = sum / entered;
+    average = sum / entered;                // Calculating average
     printf("\nAverage is: %.3f", average);
+    
     return 0;
+
 }
